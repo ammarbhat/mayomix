ee28fa24affd20c3d77f1441e577c7c722cf7e02
ammarbhat <bhatmmr@gmail.com>
2026-09-18 17:20:50 +0530

    Add taste and review models

--- src/models.py
+++ src/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, String
 from database import Base
-from typing import Optional
+from typing import List
 from datetime import datetime
 
 
@@ -11,8 +11,37 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]
     uername: Mapped[str]
-    bio: Mapped[Optional[str]]
+    bio: Mapped[str | None]
     email: Mapped[str]
     hash_password: Mapped[str]
-    pfp_link: Mapped[str]
+    pfp_link: Mapped[str | None]
     create_date: Mapped[datetime]
+
+    taste_entries = Mapped[List["TasteEntry"]] = relationship(back_populates="user")
+    reviews = Mapped[List["Review"]] = relationship(back_populates="user")
+
+
+class TasteEntry(Base):
+    __tablename__ = "taste_entry"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    mbid: Mapped[str]
+    category: Mapped[str]
+    rank: Mapped[int]
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    liked: Mapped[bool | None]
+
+    user: Mapped["User"] = relationship(back_populates="taste_entries")
+
+
+class Review(Base):
+    __tablename__ = "review"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    review_str: Mapped[str] = mapped_column(String(400))
+    rating: Mapped[int]
+    user_id: Mapped[int] = mapped_column(ForeignKey="user.id")
+    create_date: Mapped[datetime]
+    updated_date: Mapped[datetime]
+
+    user = Mapped["User"] = relationship(back_populates="reviews")
